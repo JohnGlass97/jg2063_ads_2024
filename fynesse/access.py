7353aa7fa41209a1dc5c814512dc615475bc6b6f
JohnGlass97 <83819614+JohnGlass97@users.noreply.github.com>
2024-11-28 16:16:40 +0000

    Add types, comments and cleanup code in access.py

--- fynesse/access.py
+++ fynesse/access.py
@@ -10,34 +10,27 @@ from .config import *
 # This file accesses the data
 
 
-def download_price_paid_data(year_from, year_to):
+def download_price_paid_data(year_from: int, year_to: int) -> None:
+    """Download UK house price data for given year range inclusive as CSV files."""
+
     # Base URL where the dataset is stored
     base_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com"
-    """Download UK house price data for given year range"""
-    # File name with placeholders
-    file_name = "/pp-<year>-part<part>.csv"
+
     for year in range(year_from, (year_to+1)):
         print(f"Downloading data for year: {year}")
         for part in range(1, 3):
-            url = base_url + \
-                file_name.replace("<year>", str(year)).replace(
-                    "<part>", str(part))
-            response = requests.get(url)
+            file_name = f"/pp-{year}-part{part}"
+            response = requests.get(base_url + file_name)
             if response.status_code == 200:
-                with open("." + file_name.replace("<year>", str(year)).replace("<part>", str(part)), "wb") as file:
+                local_file_name = f"./{file_name}.csv"
+                with open(local_file_name, "wb") as file:
                     file.write(response.content)
 
 
-def create_connection(user, password, host, database, port=3306):
+def create_connection(user: str, password: str, host: str, database: str, port=3306) -> pymysql.Connection | None:
     """ Create a database connection to the MariaDB database
-        specified by the host url and database name.
-    :param user: username
-    :param password: password
-    :param host: host url
-    :param database: database name
-    :param port: port number
-    :return: Connection object or None
-    """
+        specified by the host url and database name."""
+
     conn = None
     try:
         conn = pymysql.connect(user=user,
@@ -50,10 +43,22 @@ def create_connection(user, password, host, database, port=3306):
         print(f"Connection established!")
     except Exception as e:
         print(f"Error connecting to the MariaDB Server: {e}")
+
     return conn
 
 
-def housing_upload_join_data(conn, year):
+def get_df_from_query(conn: pymysql.Connection, query: str) -> pd.DataFrame:
+    """Run a query on the database and return the result as a pandas dataframe."""
+
+    cur = conn.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+    df = pd.DataFrame(rows, columns=[x[0].split(".")[-1]
+                      for x in cur.description])
+    return df
+
+
+def housing_upload_join_data(conn: pymysql.Connection, year: int) -> None:
     start_date = str(year) + "-01-01"
     end_date = str(year) + "-12-31"
 
@@ -89,7 +94,7 @@ def fetch_pois(latitude: float, longitude: float, tags: dict, distance_km: float
         gdf: A geopandas dataframe of the POIs.
     """
     point = latitude, longitude
-    radius = distance_km * 1000
+    radius = round(distance_km * 1000)
 
     features = ox.features_from_point(point, tags, radius)
     print("-", end="")  # To signify progress
@@ -115,33 +120,32 @@ def parallel_fetch_pois(locations_dict: dict, tags: dict, distance_km: float = 1
     return [a for a in zip(locations_dict.keys(), results)]
 
 
-def bbox_of(lat, long, distance_km):
-    """Returns an approx bbox of the form (N, S, E, W) with specified width and height."""
+def bbox_of(lat: float, long: float, distance_km: float) -> tuple:
+    """Returns an approx bbox of the form (N, S, E, W) with specified width and height.
+    It is only accurate at the equator"""
 
     d = distance_km / 111 / 2
+
     return (lat+d, lat-d, long+d, long-d)
 
 
-def get_price_paid_data_in_regions(conn, bbox, from_date, columns):
-    """Use provided DB connection to get price paid data within the given bounding box."""
+def get_price_paid_data_in_regions(conn: pymysql.Connection, bbox: tuple, from_date, columns) -> pd.DataFrame:
+    """Use provided DB connection to get price paid data within the given bounding box.
+    Note that the bbox is approximated and only accurate at the equator."""
 
-    cur = conn.cursor()
     subquery = f"SELECT {', '.join(columns)}, pp.postcode AS postcode, " + \
         "ROW_NUMBER() OVER (PARTITION BY pp.postcode, street, primary_addressable_object_name ORDER BY date_of_transfer DESC) AS row_num " + \
         "FROM pp_data pp JOIN postcode_data po ON pp.postcode = po.postcode " + \
         f"WHERE po.latitude BETWEEN {bbox[1]} AND {bbox[0]} AND po.longitude BETWEEN {bbox[3]} AND {bbox[2]} " + \
         f"AND date_of_transfer >= '{from_date}'"
 
-    cur.execute(f"SELECT {', '.join(columns)}, postcode FROM ({
-                subquery}) AS ranked WHERE row_num = 1;")
+    query = f"SELECT {', '.join(columns)}, postcode FROM ({
+        subquery}) AS ranked WHERE row_num = 1;"
 
-    rows = cur.fetchall()
-    df = pd.DataFrame(rows, columns=[x[0].split(".")[-1]
-                      for x in cur.description])
-    return df
+    return get_df_from_query(conn, query)
 
 
-def get_buildings_with_addresses(bbox):
+def get_buildings_with_addresses(bbox: tuple):
     """Returns OSM buildings with housenumber, street, and postcode."""
 
     tags = {
