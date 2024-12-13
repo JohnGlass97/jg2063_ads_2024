3858e3c1492c32085221a36f5e8e06401c21feac
JohnGlass97 <83819614+JohnGlass97@users.noreply.github.com>
2024-12-13 02:35:01 +0000

    Add mae_and_rmse

--- fynesse/address.py
+++ fynesse/address.py
@@ -1,1 +1,7 @@
-# This file contains code for suporting addressing questions in the data
+import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+
+def mae_and_rmse(true, pred) -> tuple:
+    """Return the mean absolute error and root mean squared error in a tuple."""
+    return mean_absolute_error(true, pred), np.sqrt(mean_squared_error(true, pred))
